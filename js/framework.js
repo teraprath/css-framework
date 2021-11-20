40b8ce0103e98335ed09c7b31f548eb16e317a45
Terry <68700354+teraprath@users.noreply.github.com>
2021-11-20 15:44:24 +0100

    Mobile Menu Finished
    + Mobile Menu

--- js/framework.js
+++ js/framework.js
@@ -1,3 +1,11 @@
-$(".mobile").click(function() {
-    $(".mobile-menu").show();
+// Show mobile menu
+$(".mobile-open").click(function() {
+    $(".mobile-open").hide(0);
+    $(".mobile-menu").show(200);
+});
+
+// Hide mobile menu
+$(".mobile-close").click(function() {
+    $(".mobile-menu").hide(200);
+    $(".mobile-open").show(200);
 });
